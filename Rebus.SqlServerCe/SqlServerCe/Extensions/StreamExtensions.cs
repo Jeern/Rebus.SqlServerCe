1c85b206efc3294a786e7369ec94baffb1c2c3dc
Jesper Niedermann <jesper@niedermann.dk>
2017-04-18 14:27:37 +0200

    Extension methods for Stream

---
+++ Rebus.SqlServerCe/SqlServerCe/Extensions/StreamExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace Rebus.SqlServerCe.Extensions
+{
+    /// <summary>
+    /// Extensions for the Stream object
+    /// </summary>
+    public static class StreamExtensions
+    {
+        /// <summary>
+        /// Converts a Stream to a Byte array
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static byte[] ToByteArray(this Stream input)
+        {
+            using (var ms = new MemoryStream())
+            {
+                input.CopyTo(ms);
+                return ms.ToArray();
+            }
+        }
+
+        /// <summary>
+        /// Converts a Stream to a Base64 string
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static string ToBase64String(this Stream input)
+        {
+            return Convert.ToBase64String(input.ToByteArray());
+        }
+    }
+}
